ed77d0169f9b5494280a9982327c3de6dd039a8a
ZKNobrega <54561781+ZKnobrega@users.noreply.github.com>
2021-03-10 16:18:36 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,27 +1,29 @@
 
+import java.io.IOException;
 import java.util.Scanner;
+
 public class Principal {
 
-    public static void main(String[] args) {
-        menu blz = new menu();
-        Scanner in = new Scanner(System.in);
-        String user, senha;
-        int x = 0;
-        
-        while(x<1){
-        System.out.println("Digite seu usuario: ");
-        user = in.nextLine();
-        System.out.println("Digite sua senha");
-        senha = in.nextLine();
-        
-        if (user.equals("unifap") && senha.equals("unifap123")) {
-            blz.menu();
-            x = 1;
-        } else {
-            System.out.println("senha errada");
-                    
-        }
-        
-    }
-}
+	public static void main(String[] args) throws IOException {
+		menu blz = new menu();
+		Scanner in = new Scanner(System.in);
+		String user, senha;
+		int x = 0;
+
+		while (x < 1) {
+			System.out.println("Digite seu usuario: ");
+			user = in.nextLine();
+			System.out.println("Digite sua senha");
+			senha = in.nextLine();
+
+			if (user.equals("unifap") && senha.equals("unifap123")) {
+				blz.menu();
+				x = 1;
+			} else {
+				System.out.println("senha errada");
+
+			}
+
+		}
+	}
 }
