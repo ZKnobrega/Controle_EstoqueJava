932b2abb49375bbf5d038f35304904571a1d4d27
ZKNobrega <54561781+ZKnobrega@users.noreply.github.com>
2021-03-10 16:17:35 -0300

    Update Contador.java

--- Contador.java
+++ Contador.java
@@ -1,92 +1,96 @@
-public class Contador
-{
-        double valorCaneta = 0.50;
-        double valorLapis = 0.40;
-        double valorPapal = 1;
-        double valorBorracha = 0.20;
-        
-        double valorCompraCaneta = 0.25;
-        double valorCompraLapis = 0.20;
-        double valorCompraPapal = 0.50;
-        double valorCompraBorracha = 0.10;
-        
-        double saldoAtual = 100;
-        double lucro;
-        double compras;
-        double lucroFinal=lucro-compras;
-        double valorTP=75.8;
-        
-        public void contVendas(double produto, int numeroDeProdutos){
-            if(produto==1){
-                this.lucro+=valorCaneta*numeroDeProdutos;
-                this.saldoAtual+=valorCaneta*numeroDeProdutos;
-            }
-            if(produto==2){
-                this.lucro+=valorLapis*numeroDeProdutos;
-                this.saldoAtual+=valorLapis*numeroDeProdutos;
+public class Contador {
+	double valorMonitor = 499.90;
+	double valorTeclado = 199.90;
+	double valorMouse = 59.90;
+	double valorFonte = 274.90;
 
-            }
-            if(produto==3){
-                this.lucro+=valorPapal*numeroDeProdutos;
-                this.saldoAtual+=valorPapal*numeroDeProdutos;
-            }
-            if(produto==4){
-                this.lucro+=valorBorracha*numeroDeProdutos;
-                this.saldoAtual+=valorBorracha*numeroDeProdutos;
-            }
-    }
-    public double saldo(){
-    System.out.println("saldo é: "+saldoAtual);
-    return saldoAtual;
-    }
-    
-    public double verificaSaldo(double qnt, double tipo){
-        if(tipo==1){
-        if((qnt*this.valorCaneta) >saldoAtual){
-        System.out.println("saldo insuficiente"); 
-        return  0; 
-    }}
-    if(tipo==2){
-        if((qnt*this.valorLapis) >saldoAtual){
-        System.out.println("saldo insuficiente"); 
-        return  0; 
-    }}
-    if(tipo==3){
-        if((qnt*this.valorPapal) >saldoAtual){
-        System.out.println("saldo insuficiente"); 
-        return  0; 
-    }}
-    if(tipo==4){
-        if((qnt*this.valorBorracha) >saldoAtual){
-        System.out.println("saldo insuficiente"); 
-        return  0; 
-    }}
-    return 1;
-}    
-    public void contCompras(double produto, int numeroDeProdutos){
-            if(produto==1){
-                this.lucro-=valorCompraCaneta*numeroDeProdutos;
-                this.saldoAtual-=valorCompraCaneta*numeroDeProdutos;
-            }
-            if(produto==2){
-                this.lucro-=valorCompraLapis*numeroDeProdutos;
-                 this.saldoAtual-=valorCompraLapis*numeroDeProdutos;
-            }
-            if(produto==3){
-                this.lucro-=valorCompraPapal*numeroDeProdutos;
-                 this.saldoAtual-=valorCompraPapal*numeroDeProdutos;
-            }
-            if(produto==4){
-                this.lucro-=valorCompraBorracha*numeroDeProdutos;
-                 this.saldoAtual-=valorCompraBorracha*numeroDeProdutos;
-            }
-    }
-    
-    public void lucro(){
-        if(this.lucro>=0){
-        System.out.println("o valor bruto da venda foi de: "+this.lucro);
-    }else{
-        System.out.println("você teve um prejuizo de: "+this.lucro);
-    }
-}   
+	double valorCompraMonitor = 150;
+	double valorCompraTeclado = 80;
+	double valorCompraMouse = 19.90;
+	double valorCompraFonte = 99.90;
+
+	double saldoAtual = 1000;
+	double lucro;
+	double compras;
+	double lucroFinal = lucro - compras;
+
+	public void contVendas(double produto, int numeroDeProdutos) {
+		if (produto == 1) {
+			this.lucro += valorMonitor * numeroDeProdutos;
+			this.saldoAtual += valorMonitor * numeroDeProdutos;
+		}
+		if (produto == 2) {
+			this.lucro += valorTeclado * numeroDeProdutos;
+			this.saldoAtual += valorTeclado * numeroDeProdutos;
+
+		}
+		if (produto == 3) {
+			this.lucro += valorMouse * numeroDeProdutos;
+			this.saldoAtual += valorMouse * numeroDeProdutos;
+		}
+		if (produto == 4) {
+			this.lucro += valorFonte * numeroDeProdutos;
+			this.saldoAtual += valorFonte * numeroDeProdutos;
+		}
+	}
+
+	public double saldo() {
+		// System.out.println("saldo é: "+saldoAtual);
+		return saldoAtual;
+	}
+
+	public double verificaSaldo(double qnt, double tipo) {
+		if (tipo == 1) {
+			if ((qnt * this.valorMonitor) > saldoAtual) {
+				System.out.println("saldo insuficiente");
+				return 0;
+			}
+		}
+		if (tipo == 2) {
+			if ((qnt * this.valorTeclado) > saldoAtual) {
+				System.out.println("saldo insuficiente");
+				return 0;
+			}
+		}
+		if (tipo == 3) {
+			if ((qnt * this.valorMouse) > saldoAtual) {
+				System.out.println("saldo insuficiente");
+				return 0;
+			}
+		}
+		if (tipo == 4) {
+			if ((qnt * this.valorFonte) > saldoAtual) {
+				System.out.println("saldo insuficiente");
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+	public void contCompras(double produto, int numeroDeProdutos) {
+		if (produto == 1) {
+			this.lucro -= valorCompraMonitor * numeroDeProdutos;
+			this.saldoAtual -= valorCompraMonitor * numeroDeProdutos;
+		}
+		if (produto == 2) {
+			this.lucro -= valorCompraTeclado * numeroDeProdutos;
+			this.saldoAtual -= valorCompraTeclado * numeroDeProdutos;
+		}
+		if (produto == 3) {
+			this.lucro -= valorCompraMouse * numeroDeProdutos;
+			this.saldoAtual -= valorCompraMouse * numeroDeProdutos;
+		}
+		if (produto == 4) {
+			this.lucro -= valorCompraFonte * numeroDeProdutos;
+			this.saldoAtual -= valorCompraFonte * numeroDeProdutos;
+		}
+	}
+
+	public void lucro() {
+		if (this.lucro >= 0) {
+			System.out.println(" o valor bruto das vendas foi de: " + this.lucro);
+		} else {
+			System.out.println("você teve um prejuizo com as vendas de: " + this.lucro);
+		}
+	}
 }
