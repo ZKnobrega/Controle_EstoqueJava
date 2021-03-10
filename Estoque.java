0a692c06ce872fc1087a8d29099dde81ffc5fad1
ZKNobrega <54561781+ZKnobrega@users.noreply.github.com>
2021-03-10 16:18:02 -0300

    Update Estoque.java

--- Estoque.java
+++ Estoque.java
@@ -1,83 +1,98 @@
-public class Estoque extends Contador
-{
-    int caneta = 120, lapis = 120, papal = 120, borracha = 120;
-    double caixasCaneta = 10, caixasLapis =10, caixasBorracha = 20;
-        
-       /* double valorTotalCaneta = (caneta * valorCaneta);
-        double valorTotalLapis = (lapis * valorLapis);
-        double valorTotalPapal =(papal * valorPapal);
-        double valorTotalBorracha = (borracha * valorBorracha);
-        */
-        
-         void exibirProdutos(int op){
-                    System.out.println("-------PRODUTOS-------");
-                    System.out.println("1-Canetas: " + this.caneta + "("+ this.caixasCaneta+") caixas");
-                    System.out.println("2-lapis: " + this.lapis + "("+this.caixasLapis+") caixas");
-                    System.out.println("3-papal: " + this.papal + " resmas");
-                    System.out.println("4-borrachas: " + this.borracha + "("+this.caixasBorracha+") caixas");
-                    
-                    if(op==1){
-                    System.out.println("qual produto deseja Comprar");
-                }if(op==2){
-                System.out.println("qual produto deseja Vender");
-                }
-        }
-        
-        public double adcCaneta(int caneta, int qnt){
-        this.caneta += qnt;
-        return this.caixasCaneta = (caneta / 10);
-    }
-     public double adcLapis(int lapis, int qnt){
-        this.lapis +=qnt;
-        return this.caixasLapis = (lapis / 12);
-    }
-     public double adcBorracha(int borracha, int qnt){
-        this.borracha += qnt;
-        return this.caixasBorracha = (borracha/ 6);
-    }
-    
-    public double verificaEstoque(double qnt, double tipo){
-        if(tipo==1){
-        if(qnt >caneta){
-        System.out.println("estoque insuficiente"); 
-        return  0; 
-    }}
-    if(tipo==2){
-        if(qnt >lapis){
-        System.out.println("estoque insuficiente"); 
-        return  0; 
-    }}
-    if(tipo==3){
-        if(qnt >papal){
-        System.out.println("estoque insuficiente"); 
-        return  0; 
-    }}
-    if(tipo==4){
-        if(qnt >borracha){
-        System.out.println("estoque insuficiente"); 
-        return  0; 
-    }}
-    return 1;
-}
-        public double vendeCaneta(int caneta, int qnt){
-        this.caneta -= qnt;
-        return this.caixasCaneta = (caneta / 10);
-    }
-     public double vendeLapis(int lapis, int qnt){
-        this.lapis -=qnt;
-        return this.caixasLapis = (lapis / 12);
-    }
-     public double vendeBorracha(int borracha, int qnt){
-        this.borracha -= qnt;
-        return this.caixasBorracha = (borracha/ 6);
-    }
-    
-    void vendOuComp(int voc){
-        if(voc==1){
-            System.out.println("Quantas unidades deseja comprar?");        
-        }if(voc==2){
-            System.out.println("Quantas unidades deseja Vender?");
-        }
-    }
-            
+import java.io.IOException;
+
+public class Estoque extends Contador {
+	int monitor = 120, teclado = 240, mouse = 50, fonte = 60;
+
+	/*
+	 * double valorTotalCaneta = (caneta * valorCaneta); double valorTotalLapis =
+	 * (lapis * valorLapis); double valorTotalPapal =(papal * valorPapal); double
+	 * valorTotalBorracha = (borracha * valorBorracha);
+	 */
+
+	void exibirProdutos(int op) throws IOException {
+		System.out.println("\n-------PRODUTOS-------");
+		System.out.println("1-MONITOR: " + this.monitor + " Unidades \n  Valor venda (" + this.valorMonitor + ") \n  Valor de produção (" + this.valorCompraMonitor + ")\n");
+		System.out.println("2-TECLADO: " + this.teclado + " Unidades \n  Valor venda (" + this.valorTeclado + ") \n  Valor de produção (" + this.valorCompraTeclado + ")\n");
+		System.out.println("3-MOUSE: " + this.mouse + " Unidades \n  Valor venda (" + this.valorMouse + ") \n  Valor de produção (" + this.valorCompraMouse + ")\n");
+		System.out.println("4-FONTE: " + this.fonte + " Unidades \n  Valor venda (" + this.valorFonte + ") \n  Valor de produção (" + this.valorCompraFonte + ")\n");
+		System.out.println("Precione ENTER para continuar....");
+		System.in.read();
+		if (op == 1) {
+			System.out.println("qual produto deseja Comprar");
+		}
+		if (op == 2) {
+			System.out.println("qual produto deseja Vender");
+		}
+	}
+
+	public void adcMonitor(int monitor, int qnt) {
+		this.monitor += qnt;
+		Scanner in = new Scanner(System.in);
+	}
+
+	public void adcTeclado(int teclado, int qnt) {
+		this.teclado += qnt;
+	}
+
+	public void adcMouse(int mouse, int qnt) {
+		this.mouse += qnt;
+	}
+
+	public void adcFonte(int fonte, int qnt) {
+		this.fonte += qnt;
+	}
+
+	public double verificaEstoque(double qnt, double tipo) {
+		if (tipo == 1) {
+			if (qnt > monitor) {
+				System.out.println("estoque insuficiente");
+				return 0;
+			}
+		}
+		if (tipo == 2) {
+			if (qnt > teclado) {
+				System.out.println("estoque insuficiente");
+				return 0;
+			}
+		}
+		if (tipo == 3) {
+			if (qnt > mouse) {
+				System.out.println("estoque insuficiente");
+				return 0;
+			}
+		}
+		if (tipo == 4) {
+			if (qnt > fonte) {
+				System.out.println("estoque insuficiente");
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+	public void vendeMonitor(int monitor, int qnt) {
+		this.monitor -= qnt;
+	}
+
+	public void vendeTeclado(int teclado, int qnt) {
+		this.teclado -= qnt;
+	}
+
+	public void vendeMouse(int mouse, int qnt) {
+		this.mouse -= qnt;
+	}
+
+	public void vendeFonte(int fonte, int qnt) {
+		this.fonte -= qnt;
+	}
+
+	void vendOuComp(int voc) {
+		if (voc == 1) {
+			System.out.println("Quantas unidades deseja produzir?");
+		}
+		if (voc == 2) {
+			System.out.println("Quantas unidades deseja Vender?");
+		}
+	}
+
 }
